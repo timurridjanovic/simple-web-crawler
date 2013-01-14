f871f541ba58b76a4584df982d4bb46d5c50bf7f
Timur Ridjanovic <timur.ridjanovic@gmail.com>
2013-01-13 21:45:50 -0500

    simple web crawler in python

---
+++ web_crawler.py
@@ -0,0 +1,48 @@
+def get_page(url):
+    try:
+        import urllib
+        return urllib.urlopen(url).read()
+    except:
+        return ""
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1: 
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+
+
+def get_all_links(page):
+    links = []
+    while True:
+        url,endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+def crawl_web(seed, max_pages):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled and len(crawled) < max_pages:
+            union(tocrawl, get_all_links(get_page(page)))
+            crawled.append(page)
+    return crawled
+    
+
+print crawl_web('http://xkcd.com/353', 10)
+
+
+
